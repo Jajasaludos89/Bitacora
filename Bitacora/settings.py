c77a903217d3f4420040616efdc71e6b4c35895c
Jajasaludos89 <brandon.pilco3401@utc.edu.ec>
2025-07-22 13:05:39 -0500

    render 9.0

--- Bitacora/settings.py
+++ Bitacora/settings.py
@@ -138,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kathy.jim06.cc@gmail.com'
-EMAIL_HOST_PASSWORD = 'pdxe ywzy mggf knfc'
+EMAIL_HOST_USER = 'luckysansy89@gmail.com'                     
+EMAIL_HOST_PASSWORD = 'aqyg rtna sbxu jwcy'  
 
