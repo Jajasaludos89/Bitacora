2ea56e5c29f917abf986eeef043590638c98286c
Jajasaludos89 <brandon.pilco3401@utc.edu.ec>
2025-07-22 13:53:04 -0500

    render 10.0

--- Bitacora/settings.py
+++ Bitacora/settings.py
@@ -138,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'luckysansy89@gmail.com'                     
+EMAIL_HOST_USER = 'luckysansy89@gmail.com'  # correo de administrador                   
 EMAIL_HOST_PASSWORD = 'aqyg rtna sbxu jwcy'  
 
